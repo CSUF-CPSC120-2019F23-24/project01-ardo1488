8f1ca5b953b0c66eb1eaaa23d77be6f98d8116a5
Eduardo Gaxiola <ard01488@csu.fullerton.edu>
2019-09-07 15:36:18 -0700

    Make a program that displays a business trip location, total days, hotel expenses, meal expenses, and calculate the total expenses

--- main.cpp
+++ main.cpp
@@ -2,14 +2,16 @@
 // This program calculates and displays business expenses.
 
 #include <iostream>
+#include <string>
+#include <iomanip>
 int main()
 {
   // Welcome statement.
   std::cout << "Welcome to the Business Trip Tracker!\n";
 
 // variables.
-double location, days, hotel, meal;
-
+double days, hotel, meal;
+std::string location;
 // location of the trip.
 std::cout << "What is the business trip \"location\"?\n";
 std::cin >> location;
@@ -29,8 +31,17 @@ std::cin >> meal;
 // variable for total trip expenses.
 double total = hotel + meal;
 // display trip overview and total expenses.
-std::cout << " Location\t Days\t Hotel\t Meal\t Total\t ";
+std::cout << std::setw(8) << "Location"
+          << std::setw(8) << "Days"
+          << std::setw(8) << "Hotel"
+          << std::setw(8) << "Meal"
+          << std::setw(8) << "Total\n";
 
+std::cout << std::setw(8) <<location
+          << std::setw(8) <<days
+          << std::setw(8) <<hotel
+          << std::setw(8) <<meal
+          << std::setw(8) <<total << std::endl;
   /* code */
   return 0;
 }
