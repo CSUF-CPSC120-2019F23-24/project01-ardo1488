3030513d9eede61e8133ad8de9cf10f9aaa3aed1
Eduardo Gaxiola <ard01488@csu.fullerton.edu>
2019-09-28 16:08:08 -0700

    fix project 1 to display dollar signs and two spaces right to the decimal

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 #include <iostream>
 #include <string>
 #include <iomanip>
+#include <sstream>
 int main()
 {
   // Welcome statement.
@@ -12,40 +13,42 @@ int main()
 // variables.
 double days, hotel, meal;
 std::string location;
+std::string dollar = "$";
 // location of the trip.
-std::cout << "What is the business trip \"location\"?\n";
-std::cin >> location;
-
+std::cout << "What is the business trip \"location\"?";
+getline(std::cin, location);
 // amount of days on the trip.
-std::cout << "How many \"days\" will the trip take? \n";
+std::cout << "\nHow many \"days\" will the trip take?";
 std::cin >> days;
-
+//std::cin.ignore();
 // total hotel expense.
-std::cout << "What is the total \"hotel\" expense? \n";
+std::cout << "\nWhat is the total \"hotel\" expense?$";
 std::cin >> hotel;
-
+//std::cin.ignore();
 // total meal expenses.
-std::cout << "What is the total \"meal\" expense? \n";
+std::cout << "\nWhat is the total \"meal\" expense?$";
 std::cin >> meal;
-
+//std::cin.ignore();
 // variable for total trip expenses.
 double total = hotel + meal;
-// display trip overview and total expenses.
-std::cout << std::setw(8) << "Location"
-          << std::setw(8) << "Days"
-          << std::setw(8) << "Hotel"
-          << std::setw(8) << "Meal"
-          << std::setw(8) << "Total\n";
 
-std::cout << std::setw(8) <<location
-          << std::setw(8) <<days
-          << std::setw(8) <<hotel
-          << std::setw(8) <<meal
-          << std::setw(8) <<total << std::endl;
+std::cout << "\n";
+std::stringstream sshotel, ssmeal, sstotal;
+sshotel << dollar << std::fixed << std::setprecision(2) << hotel;
+ssmeal << dollar << std::fixed << std::setprecision(2) << meal;
+sstotal << dollar << std::fixed << std::setprecision(2) << total;
+// display trip overview and total expenses.
+std::cout <<std::left<< std::setw(20) << "Location"
+          <<std::right << std::setw(20) << "Days"
+          << std::setw(20) << "Hotel"
+          << std::setw(20) << "Meal"
+          << std::setw(20) << "Total"<< '\n';
+
+std::cout << std::left << std::setw(20) << location
+          << std::right << std::setw(20) << days
+          << std::setw(20)<< sshotel.str()
+          << std::setw(20) << ssmeal.str()
+          << std::setw(20) << sstotal.str() << std::endl;
   /* code */
   return 0;
 }
-
-
-
-// Location
